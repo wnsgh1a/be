c5d099161c10c0d41553ed9eabb21a1c726cb78a
wnsgh1a <wnsgh1a@naver.com>
2026-02-11 19:35:24 +0900

    설정: CORS 설정을 위한 WebConfig 파일 추가

--- test-backend/src/main/java/com/test/test_backend/config/WebConfig.java
+++ test-backend/src/main/java/com/test/test_backend/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.test.backend.config;
+package com.test.test_backend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
